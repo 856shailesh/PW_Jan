16da0a50b54fade41230e4131866a7a7853d12af
shaileshkumar <856shailesh@gmail.com>
2026-06-23 21:43:02 +0530

    api TC

---
+++ tests/api/users.api.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from '@playwright/test';
+
+let AUTH_TOKEN = { Authorization: 'Bearer ce3cf7c512b55c5b07b770f58bfdfa2465c1499e24395605b46f8809abd58b63' };
+
+
+test('get user test', async ({ request }) => {
+    let response = await request.get('https://gorest.co.in/public/v2/users', {
+        headers: AUTH_TOKEN
+    });
+    //console.log(response);
+    let jsonBody = await response.json();
+    console.log(jsonBody);
+
+    console.log(response.status());
+    console.log(response.statusText());
+})
+
+test('create a user test', async ({ request }) => {
+    //JS Object
+    let userData = {
+        name: 'Shai',
+        email: `automation_${Date.now()}@open.com`,
+        gender: 'male',
+        status: 'inactive'
+    };
+
+    //JS object to JSON : Serialization(Auto serialization)
+    let response = await request.post('https://gorest.co.in/public/v2/users/8516586', {
+        headers: AUTH_TOKEN,
+        data: userData
+    });
+    //console.log(response);
+    let jsonBody = await response.json();
+    console.log(jsonBody);
+
+    console.log(response.status()); // 201
+    console.log(response.statusText()); // Created
+})
