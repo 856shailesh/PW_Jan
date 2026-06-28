715fd6bde182ccc6a65436329276ef630650e860
Shailesh Kumar <shaileshkumar@Shaileshs-MacBook-Air.local>
2026-06-28 08:35:51 +0530

    git

---
+++ tests/api/users.api.indi.spec.ts
@@ -0,0 +1,68 @@
+import { get } from 'node:http';
+import { ApiHelper } from '../../src/api/ApiHelper';
+import { test, expect } from '../../src/fixtures/apifixtures';
+
+const TOKEN = process.env.API_Token!;
+let AUTH_HEADER = { Authorization: `Bearer ${TOKEN}` };
+
+//Post - get
+//Post - put
+//Post - delete 
+// Common thing is post
+async function createUser(apiHelper: any) {
+    let userData = {
+        name: 'Shailesh API',
+        email: `automation_${Date.now()}@open.com`,
+        gender: 'male',
+        status: 'inactive'
+    };
+    let response = await apiHelper.post('/public/v2/users', userData, AUTH_HEADER);
+    expect(response.status).toBe(201);
+    return response.body;
+}
+
+//Test1 : Create a user + verify
+//Post > userID > Get/userId > verify
+test('POST - create a user', async ({ apiHelper }) => {
+    //create a user:
+    let userResponse = await createUser(apiHelper);
+    //get the user
+    let response = await apiHelper.get(`/public/v2/users/${userResponse.id}`, AUTH_HEADER);
+    expect(response.status).toBe(200);
+    expect(response.body.name).toBe('Shailesh API');
+})
+
+//Test 2 : update a user
+//Post > userID > put > get/userID > verify
+test('Update a user', async ({ apiHelper }) => {
+    //create a user:
+    let userResponse = await createUser(apiHelper);
+    //get the user
+    let userUpdatedData = {
+        name: 'Shailesh API Updated',
+        status: 'inactive'
+    };
+    let response = await apiHelper.put(`/public/v2/users/${userResponse.id}`,userUpdatedData , AUTH_HEADER);
+    expect(response.status).toBe(200);
+    expect(response.body.name).toBe(userUpdatedData.name);
+    expect(response.body.status).toBe(userUpdatedData.status);
+
+    let getResponse = await apiHelper.get(`/public/v2/users/${userResponse.id}`, AUTH_HEADER);
+    expect(getResponse.status).toBe(200);
+    expect(getResponse.body.name).toBe(userUpdatedData.name);
+})
+
+//Test 3 : Delete a user
+// Post > delete > get
+test('Delete a user', async ({ apiHelper }) => {
+    //create a user:
+    let userResponse = await createUser(apiHelper);
+    //get the user
+    
+    let response = await apiHelper.delete(`/public/v2/users/${userResponse.id}` , AUTH_HEADER);
+    expect(response.status).toBe(204);
+
+    let getResponse = await apiHelper.get(`/public/v2/users/${userResponse.id}`, AUTH_HEADER);
+    expect(getResponse.status).toBe(404);
+    expect(getResponse.body.message).toBe('Resource not found');
+})
--- tests/api/users.api.sep.spec.ts
+++ tests/api/users.api.sep.spec.ts
@@ -5,35 +5,41 @@ let AUTH_HEADER = { Authorization: `Bearer ${TOKEN}` };
 
 let userId: number;
 
-//GET test:
-test('GET --- get all users', async ({ apiHelper }) => {
-    let response = await apiHelper.get('/public/v2/users', AUTH_HEADER);
-    expect(response.status).toBe(200);
-    expect(response.body.length).toBeGreaterThan(0);
-})
+test.describe.serial('running e2e go rest curd api test', () => {
+    //GET test:
+    test('GET --- get all users', async ({ apiHelper }) => {
+        let response = await apiHelper.get('/public/v2/users', AUTH_HEADER);
+        expect(response.status).toBe(200);
+        expect(response.body.length).toBeGreaterThan(0);
+    })
 
-test('POST API --- create a user', async ({ apiHelper }) => {
-    let userData = {
-        name: 'Shailesh API',
-        email: `automation_${Date.now()}@open.com`,
-        gender: 'male',
-        status: 'inactive'
-    };
-    let response = await apiHelper.post('/public/v2/users', userData, AUTH_HEADER);
-    expect(response.status).toBe(201);
-    expect(response.body.name).toBe(userData.name);
-    userId = response.body.id;
-    console.log('Created user id ', userId);
-})
+    test('POST API --- create a user', async ({ apiHelper }) => {
+        let userData = {
+            name: 'Shailesh API',
+            email: `automation_${Date.now()}@open.com`,
+            gender: 'male',
+            status: 'inactive'
+        };
+        let response = await apiHelper.post('/public/v2/users', userData, AUTH_HEADER);
+        expect(response.status).toBe(201);
+        expect(response.body.name).toBe(userData.name);
+        userId = response.body.id;
+        console.log('Created user id ', userId);
+    })
 
-test('UPDATE API --- update a user', async ({ apiHelper }) => {
-    let userUpdatedData = {
-        name: 'Shailesh API Updated',
-        status: 'inactive'
-    };
-    let response = await apiHelper.post(`/public/v2/users/${userId}`, userUpdatedData, AUTH_HEADER);
-    expect(response.status).toBe(201);
-    expect(response.body.name).toBe(userUpdatedData.name);
-    let userId = response.body.id;
-    console.log('Created user id ', userId);
-})
+    test('PUT API --- update a user', async ({ apiHelper }) => {
+        let userUpdatedData = {
+            name: 'Shailesh API Updated',
+            status: 'inactive'
+        };
+        let response = await apiHelper.put(`/public/v2/users/${userId}`, userUpdatedData, AUTH_HEADER);
+        expect(response.status).toBe(200);
+        expect(response.body.name).toBe(userUpdatedData.name);
+        expect(response.body.status).toBe(userUpdatedData.status);
+    })
+
+    test('DELETE API --- update a user', async ({ apiHelper }) => {
+        let response = await apiHelper.delete(`/public/v2/users/${userId}`, AUTH_HEADER);
+        expect(response.status).toBe(404);
+    })
+});
